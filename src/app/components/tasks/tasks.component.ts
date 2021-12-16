e3844218b05a8bfd4661569cb5cf4ee056d5b421
andrecmatias <andre.cmatias@gmail.com>
2021-12-16 16:33:25 +0000

    Toggle Reminder

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -17,11 +17,18 @@ export class TasksComponent implements OnInit {
     this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
   }
 
-  deleteTask(task: any) {
+  deleteTask(task: Task) {
     this.taskService
       .deleteTask(task)
       .subscribe(
         () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
       );
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService
+      .updateTaskReminder(task)
+      .subscribe();
   }
 }
