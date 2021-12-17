44e0d1317b239cbb892cdb41e0570045bc3e9edd
andrecmatias <andre.cmatias@gmail.com>
2021-12-17 09:48:28 +0000

    Submit & Create Task

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -32,4 +32,9 @@ export class TaskService {
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.put<Task>(url, task, httpOptions);
   }
+
+  addTask (task: Task): Observable<Task> {
+    const url = `${this.apiUrl}`;
+    return this.http.post<Task>(url, task, httpOptions);
+  }
 }
